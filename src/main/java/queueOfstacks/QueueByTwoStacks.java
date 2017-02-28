31eefc1e19550895f88c28f9534afd3c919db6ea
Egor Kozyrev <towel98@yandex.ru>
2017-02-28 21:51:22 +0300

    Queu task

--- src/main/java/queueOfstacks/QueueByTwoStacks.java
+++ src/main/java/queueOfstacks/QueueByTwoStacks.java
@@ -1,7 +1,32 @@
 package queueOfstacks;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Created by Egor on 28-Feb-17.
  */
 public class QueueByTwoStacks {
+    private Deque<Object> input = new LinkedList<>();
+    private Deque<Object> output = new LinkedList<>();
+
+    public void enqueue(Integer element) {
+        input.push(element);
+    }
+
+    public Object dequeue() {
+        if (output.isEmpty()) {
+            refillOutput();
+            if (output.isEmpty()) {
+                return null;
+            }
+        }
+        return output.pop();
+    }
+
+    private void refillOutput() {
+        while (!input.isEmpty()) {
+            output.push(input.pop());
+        };
+    }
 }
--- src/test/java/queueOfStacks/QueueByTwoStacksTest.java
+++ src/test/java/queueOfStacks/QueueByTwoStacksTest.java
@@ -1,7 +1,26 @@
 package queueOfStacks;
 
+import org.junit.Assert;
+import org.junit.Test;
+import queueOfstacks.QueueByTwoStacks;
+
 /**
  * Created by Egor on 28-Feb-17.
  */
 public class QueueByTwoStacksTest {
+
+    QueueByTwoStacks queue = new QueueByTwoStacks();
+
+    @Test
+    public void testInputAndOutput() {
+        queue.enqueue(1);
+        queue.enqueue(2);
+        Assert.assertEquals(1, queue.dequeue());
+        queue.enqueue(3);
+        queue.enqueue(4);
+        Assert.assertEquals(2, queue.dequeue());
+        Assert.assertEquals(3, queue.dequeue());
+        Assert.assertEquals(4, queue.dequeue());
+        Assert.assertEquals(null, queue.dequeue());
+    }
 }
